e2c4835de3f916d1427e7950c91759d550ae6598
TvT <chenj24885@masseyhigh.school.nz>
2026-03-10 10:12:37 +1300

    Statement generator

---
+++ C_05_Statement_Generator..py
@@ -0,0 +1,13 @@
+def make_statement(statement, decoration):
+    """Adds emoji / additional characters to the start and end of headings"""
+
+    ends = decoration * 3
+    print(f"{ends} {statement} {ends}")
+
+
+
+    make_statement("I love Python","❤️" )
+    make_statement("Round Results","=")
+
+
+
